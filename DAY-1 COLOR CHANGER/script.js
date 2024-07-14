e229ce307ac4f52ea33ae6167d64e03dfe473817
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2024-07-14 20:19:48 +0530

    day 1 complite

--- DAY-1 COLOR CHANGER/script.js
+++ DAY-1 COLOR CHANGER/script.js
@@ -4,8 +4,28 @@ const body = document.querySelector('body');
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         const color = e.target.id;
-        if (['blue', 'skyblue', 'pink', 'yellow'].includes(color)) {
+        if (['blue', 'aqua', 'pink', 'yellow'].includes(color)) {
             body.style.backgroundColor = color;
         }
     });
 });
+
+
+
+// OR 
+// document.getElementById('yellow').addEventListener('click', function() {
+//     document.body.style.backgroundColor = 'yellow';
+// });
+
+// document.getElementById('blue').addEventListener('click', function() {
+//     document.body.style.backgroundColor = 'blue';
+// });
+
+// document.getElementById('skyblue').addEventListener('click', function() {
+//     document.body.style.backgroundColor = 'aqua';
+// });
+
+// document.getElementById('pink').addEventListener('click', function() {
+//     document.body.style.backgroundColor = 'pink';
+// });
+
