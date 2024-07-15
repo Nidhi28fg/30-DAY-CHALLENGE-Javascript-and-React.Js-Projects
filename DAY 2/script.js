9a081dc77c0890c2062f63dcfa3c5f12cb851f4e
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2024-07-15 09:13:45 +0530

    some change

--- DAY 2/script.js
+++ DAY 2/script.js
@@ -1,22 +1,19 @@
-const forms = document.querySelectorAll("form")
+const forms = document.querySelectorAll("form");
 
-forms.addEventLister('submit', function(e) {
+forms.addEventLister('submit', function (e) {
     e.preventDefault();
+    const height = parseInt(document.querySelector(".height").value);
+    const weight = parseInt(document.querySelector(".weight").value);
+    const result = document.querySelector(".result");
 
-    const height = document.getElementsByClassName(".height");
-    const weight = document.getElementsByClassName(".weight");
-    const result = document.getElementsByClassName(".result");
-
-    if (heightInput.value ==="" || heightInput.value <= 0 || isNaN(heightInput.value)){
-        return Please put valid number
+    if (height === "" || height <= 0 || isNaN(height)){
+        result.innerHTML=` Please put valid number ${height};
     }else if (weightInput.value ==="" || weightInput.value <= 0 || isNaN(weightInput.value)){
-        return Please put valid number
+        result.innerHTML=` Please put valid number ${weight};
     } else {
-        const finalresult = (weight / {height / height} x 10000) toFixed(2);;
-        return result.innerHtml = finalresult
+        const finalresult = (weight / ((height*height)/10000)).toFixed(2);
+        result.innerHTML=` <span> ${finalresult} </span>`;
     }
 
 
-}
-
-)
+});
