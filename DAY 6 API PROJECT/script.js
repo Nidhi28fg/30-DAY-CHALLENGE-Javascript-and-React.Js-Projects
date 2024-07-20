9a4ea6417f817c64109b652083acd26873951526
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2024-07-20 16:31:44 +0530

    add some other change

--- DAY 6 API PROJECT/script.js
+++ DAY 6 API PROJECT/script.js
@@ -3,17 +3,29 @@ const ndetails = new XMLHttpRequest();
 ndetails.open('GET', nidhidetails);
 ndetails.onreadystatechange = function () {
     console.log(ndetails.readyState);  
-    if (ndetails.readyState === 4){
+    if (ndetails.readyState === 4 && ndetails.status === 200){
         const data = JSON.parse(this.responseText);
         console.log(data.followers);
         const img = document.createElement('img')
         img.src=data.avatar_url;
         img.alt="Profile Picture";
         img.class="profile-pic";
-        document.createElement('h2').innerHTML= data.name;
-        
-        document.querySelector('.card').appendChild(img);
+        const h2 = document.createElement('h2')
+        h2.innerHTML=`${data.name}`;
+        h2.className= "name";
+       
 
+        const p = document.createElement('p');
+        p.innerHTML= `<p>Followers: ${data.followers} <br>
+        Following: ${data.following} <br>
+        Location: ${data.location} <br>
+        UserId: ${data.login}<br>
+        Id: ${data.id}<br>
+        </p>`
+
+ document.querySelector('.card').appendChild(img);
+        document.querySelector('.card').appendChild(h2);
+        document.querySelector('.card').appendChild(p);
     } 
 }
 ndetails.send();
