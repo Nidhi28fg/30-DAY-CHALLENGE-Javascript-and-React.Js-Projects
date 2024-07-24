6f27acdb28ff21bbb6ce7c16b114415d683df16c
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2024-07-24 22:41:40 +0530

    count 0 to 20

--- day-9/src/App.js
+++ day-9/src/App.js
@@ -1,6 +1,28 @@
+import React, { useState } from "react";
+
+
+
 function App() {
+  let [counter, setCounter] = useState(16);
+
+  const add = () => {
+    if (counter < 20){
+      setCounter( counter + 1 )
+    }
+  }
+
+  const ruduce = () => {
+    if ( counter > 0){
+    setCounter( counter - 1 )}
+  }
+
+
   return (
-   <h1>NIDHI WEB DEVELOPER</h1>
+    <>
+   <h1>You can add number 0 to 20</h1>
+   <button onClick={add}> Add Number {counter}</button>
+   <button onClick={ruduce}> Reduce Number {counter}</button>
+</>
   );
 }
 
