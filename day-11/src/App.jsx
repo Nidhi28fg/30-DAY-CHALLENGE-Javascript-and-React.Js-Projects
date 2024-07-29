4daac46aa08084ae168c3bb476abf27df79240f0
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2024-07-29 14:35:28 +0530

    describe

--- day-11/src/App.jsx
+++ day-11/src/App.jsx
@@ -8,29 +8,47 @@ function App() {
 
   //useRef hook
   const passwordRef = useRef(null)
+  //useRef doesn’t cause a component to re-render when the value or state changes. 
+  //Essentially, useRef is like a “box” that can hold a mutable value in its (.current) property.
+  //useRef allows referencing DOM elements.
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
+    // where our password will create
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    // it help to create new password
     if (numberAllowed) str += "0123456789"
+    // 
     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
+      // it help to create index
       pass += str.charAt(char)
+      // then charAt(index value) of str add in pass 
     }
     setPassword(pass)
+    // pass update thourgh setPassword
   }, [length, numberAllowed, charAllowed, setPassword])
+  // the function will only be recreated if length, numberAllowed, charAllowed, or setPassword change.
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
+    // The ?. optional chaining operator ensures that the code doesn't throw an error 
+    // if passwordRef.current is null or undefined.
     passwordRef.current?.setSelectionRange(0, 999);
+    //Sets the selection range
     window.navigator.clipboard.writeText(password)
+    // Writes the password string to the system clipboard.
   }, [password])
+  // Selects the entire text content within the element referenced by passwordRef.
 
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
+  // The useEffect hook calls passwordGenerator to generate an initial password.
+
+
   return (
     
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -90,4 +108,4 @@ function App() {
   )
 }
 
-export default Appls
+export default App
