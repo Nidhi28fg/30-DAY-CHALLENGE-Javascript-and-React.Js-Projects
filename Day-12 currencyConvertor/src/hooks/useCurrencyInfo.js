aa6b9e63f91895bfd17e0ab3f5f87425095047d5
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-03 07:33:33 +0530

    Update useCurrencyInfo.js

--- Day-12 currencyConvertor/src/hooks/useCurrencyInfo.js
+++ Day-12 currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -4,8 +4,8 @@ import {useEffect, useState} from "react"
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/curr encies/${currency}.json`)
+        .then((res) => res.json()) //In res, data is stored as a string. res.json() helps convert it into a JSON object. 
         .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency])
