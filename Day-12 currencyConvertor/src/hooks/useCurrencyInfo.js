8297f336be63677c9ff5c8ec33ecd63511988e7b
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-03 07:45:34 +0530

    Update useCurrencyInfo.js

--- Day-12 currencyConvertor/src/hooks/useCurrencyInfo.js
+++ Day-12 currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -2,12 +2,13 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
+    const [data, setData] = useState({}) // We create an empty object here if there is no data in useState. This prevents the app from crashing.
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/curr encies/${currency}.json`)
         .then((res) => res.json()) //In res, data is stored as a string. res.json() helps convert it into a JSON object. 
-        .then((res) => setData(res[currency]))
-        console.log(data);
+        .then((res) => setData(res[currency])) // Now in res, data is stored as a json object.
+        //All data is passed in SetData. Now setData(res[currency]) helps to access the currency data.
+            console.log(data);
     }, [currency])
     console.log(data);
     return data
