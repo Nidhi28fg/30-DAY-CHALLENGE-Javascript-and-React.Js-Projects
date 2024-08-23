05fb0820e527ce4a7dbfe88ecc987c999aa0aeaa
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-23 16:22:59 +0530

    Update User.jsx

--- day-16 reactRouter/src/components/User/User.jsx
+++ day-16 reactRouter/src/components/User/User.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import { useParams } from 'react-router-dom' 
+// The useParams function is used to get the value of the userid parameter. This parameter is passed in the URL.
+//It helps to get dynamic data
 function User() {
     const {userid} = useParams()
   return (
