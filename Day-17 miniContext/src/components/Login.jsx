a7793878121d897db0ebf4ab0fcf01f7b0e40454
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-25 15:21:03 +0530

    Update Login.jsx

--- Day-17 miniContext/src/components/Login.jsx
+++ Day-17 miniContext/src/components/Login.jsx
@@ -5,18 +5,21 @@ function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const {setUser} = useContext(UserContext)
+    const {setUser} = useContext(UserContext) 
+    // setUser is a method that is used to send data. 
+    // The data is sent in the form of a user object, which contains the username and password.
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setUser({username, password})
+        setUser({username, password}) // This is a method to send data. 
+        //The data is sent in the form of a user object, which contains the username and password.
     }
   return (
     <div>
         <h2>Login</h2>
         <input type='text'
-        value={username}
-        onChange={(e) => setUsername(e.target.value) }
+        value={username} // When you write "username" in the value field, the useState hook makes a connection with the value.
+        onChange={(e) => setUsername(e.target.value) } // onChange is an event handler that listens for changes to the value of an input field. In this case, onChange is being used to change the value of the username field.
         placeholder='username'  />
         {" "}
         <input type='text' 
