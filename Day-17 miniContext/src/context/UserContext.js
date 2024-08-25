a227ae5cbcad636d1821c159e9e5c68db2163996
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-25 13:14:33 +0530

    Update UserContext.js

--- Day-17 miniContext/src/context/UserContext.js
+++ Day-17 miniContext/src/context/UserContext.js
@@ -2,5 +2,8 @@ import React from 'react'
 
 const UserContext = React.createContext()
 
+   // react.createcontext is used for creating a context. It's a pure JavaScript component that wraps a React component.
+  // The context provides a variable, which is called a provider.
 export default UserContext;
 
+// The UserContext is a pure JavaScript component. That's why its name is UserContext.js.
