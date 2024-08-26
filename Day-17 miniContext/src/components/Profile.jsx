435938c7eacc9d6af151bcc5615ac7d926b2ae31
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-26 12:08:14 +0530

    Update Profile.jsx

--- Day-17 miniContext/src/components/Profile.jsx
+++ Day-17 miniContext/src/components/Profile.jsx
@@ -3,7 +3,9 @@ import UserContext from '../context/UserContext'
 
 function Profile() {
     const {user} = useContext(UserContext)
-    // user helps to take data 
+    // The user variable is used to retrieve data from the UserContext. 
+    // If the user variable is null, then the component displays a message asking the user to log in. 
+    // Otherwise, the component displays a message welcoming the user by name.
     
     if (!user) return <div>please login</div>
 
