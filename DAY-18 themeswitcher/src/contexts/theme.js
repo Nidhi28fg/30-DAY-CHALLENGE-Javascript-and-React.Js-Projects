ccae23d30848737e5f7170f58f0dc6932a63f6a6
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-28 13:39:57 +0530

    Update theme.js

--- DAY-18 themeswitcher/src/contexts/theme.js
+++ DAY-18 themeswitcher/src/contexts/theme.js
@@ -1,11 +1,13 @@
 import { createContext, useContext } from "react";
-
 export const ThemeContext = createContext({
-    themeMode: "light",
+    themeMode: "light", //In the above code, you can take a variable and function in the inside value. This is useful when you want to share data between components.
     darkTheme: () => {},
     lightTheme: () => {},
 })
-
+//This code creates a context object that is used to share data between components. 
+// The context object is created using the createContext method. 
+//The context object is then provided to the ThemeProvider component. 
+//The ThemeProvider component is used to provide the context object to the components that need it.
 export const ThemeProvider = ThemeContext.Provider
 
 export default function useTheme(){
