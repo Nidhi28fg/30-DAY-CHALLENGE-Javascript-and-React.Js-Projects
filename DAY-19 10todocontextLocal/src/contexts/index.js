9c268ad85e4e3bffb51c93dc931f5d7d26fa8e32
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-08-30 16:14:39 +0530

    Update index.js

--- DAY-19 10todocontextLocal/src/contexts/index.js
+++ DAY-19 10todocontextLocal/src/contexts/index.js
@@ -1,1 +1,4 @@
 export {TodoContext, TodoProvider, useTodo} from "./TodoContext"
+
+// Index.js is a JavaScript file that combines all the files in a project into a single file. 
+// This makes it easier to work with all the files in a project.
