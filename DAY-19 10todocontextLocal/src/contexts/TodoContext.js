de08e5e101072dbc2e184e582e6b67a010fb80c0
Nidhi Singh Bhadoria <165938813+Nidhi28fg@users.noreply.github.com>
2024-09-04 16:15:23 +0530

    Update TodoContext.js

--- DAY-19 10todocontextLocal/src/contexts/TodoContext.js
+++ DAY-19 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,14 +1,14 @@
 import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-    todos: [
+    todos: [                    //This is the todos list that is shown on the dashboard. It contains a list of todo items, each of which has an id, a todo description, and a completion status.
         {
             id: 1,
             todo: " Todo msg",
             completed: false,
         }
     ],
-    addTodo: (todo) => {},
+    addTodo: (todo) => {},        
     updateTodo: (id, todo) => {},
     deleteTodo: (id) => {},
     toggleComplete: (id) => {}
